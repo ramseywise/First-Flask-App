473b8ee240b8616b8757ca77c34d61072e740c11
Elizabeth Wise <elizabethwise@Elizabeth-Wise.local>
2019-11-28 11:52:57 +0100

    add grainnoush page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     """Return home webpage."""
 
+    return render_template("home.html")
+
+
+@app.route("/grainnoush")
+def grainnoush():
     return "Hello, Grainnoush!"
 
 
