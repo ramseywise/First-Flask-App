55c43ceb619c4b16369f47f19f829c68f2b257b5
Elizabeth Wise <elizabethwise@Elizabeth-Wise.local>
2019-11-28 20:02:56 +0100

    add list

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from flask import Flask, render_template, request
+import random
 
-# App config.
-DEBUG = True
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config["SECRET_KEY"] = "7d441f27d441f27567d441f2b6176a"
 
 
 @app.route("/")
@@ -15,27 +11,72 @@ def home():
     return render_template("home.html")
 
 
-class ReusableForm(Form):
-    name = TextField("Entry:", validators=[validators.required()])
+# @app.route("/form")
+# def form():
+#     """Return form submission webpage."""
 
-    @app.route("/grainnoush", methods=["GET", "POST"])
-    def grainnoush():
-        """Return hello Grainnoush."""
+#     return render_template("form.html", form=form)
 
-        form = ReusableForm(request.form)
+# @app.route("/form", methods=["POST"])
+# def my_form_post():
+#     text = request.form["text"]
+#     processed_text = text.upper()
+#     return processed_text
 
-        print form.errors
-        if request.method == "POST":
-            entry = request.form["entry"]
-            print name
+rammoush_list = [
+    "Just being a dork",
+    "Who are you and why are you sitting at Grainne’s desk?",
+    "My fearless leader",
+    "Grainnnoush oush oush begoush",
+    "Well done lady!",
+    "My official name is Lady Dr. Frau Elizabeth Ramsey Lupita Martinez Baranca Kukuktshka Bass Wise",
+    "I’m a doctor, bitch",
+    "Whoooop whoop",
+    "Sneaky Snakey",
+    "I got you boo!",
+    "Fuzzy Wuzzy",
+    "Declean and rebase",
+    "Are we meeting now?",
+    "#ourfireisbiggerthanyours",
+    "I don’t have permissions",
+    "Feel better babe!",
+    "Merci buckets",
+    "You  can run, but you can’t escape Macau",
+    "Data ladies, fuck yeah",
+    "Let’s pp together",
+    "DWH had a little meltdown",
+    "Virtual hug",
+    "Paw paw paw paw at my <3",
+    "Fifty shades of Gondor",
+    "My Irish Southern Belle",
+    "The Macau-AML-Plumbum Love Child",
+    "You killed it, G",
+    "Panda eats, shoots and leaves",
+    "Dungeon Demon",
+    "Get stuff FUN day",
+    "Hey squirrel friend",
+    "We’re gonna catch us some frauds!",
+    "Bokey bokeh",
+    "OMG I love this pussy!",
+    "Prrrrrrrrrfect ",
+    "Yaaaaaasss queen",
+]
 
-        if form.validate():
-            # Save the comment here.
-            flash("Try out my Rammoush bot!" + entry)
-        else:
-            flash("All the form fields are required. ")
 
-        return render_template("grainnoush.html", form=form)
+@app.route("/kitty")
+def kitty():
+    """Return random kitty webpage."""
+
+    insert = random.choice(rammoush_list)
+
+    return render_template("kitty.html")
+
+
+@app.route("/haha")
+def haha():
+    """Return prank webpage."""
+
+    return render_template("haha.html")
 
 
 if __name__ == "__main__":
