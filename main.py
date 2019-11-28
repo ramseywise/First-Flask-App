7b2aba3a70ce68e1c79ba56b63e1f9a42b1b9a7c
Elizabeth Wise <elizabethwise@Elizabeth-Wise.local>
2019-11-28 12:45:10 +0100

    add entry logger field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
+# App config.
+DEBUG = True
 app = Flask(__name__)
 
 
@@ -10,10 +13,27 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/grainnoush")
-def grainnoush():
-    """Return hello Grainnoush."""
-    return render_template("grainnoush.html")
+class ReusableForm(Form):
+    name = TextField("Entry:", validators=[validators.required()])
+
+    @app.route("/grainnoush", methods=["GET", "POST"])
+    def grainnoush():
+        """Return hello Grainnoush."""
+
+        form = ReusableForm(request.form)
+
+        print form.errors
+        if request.method == "POST":
+            entry = request.form["entry"]
+            print name
+
+        if form.validate():
+            # Save the comment here.
+            flash("Try out my Rammoush bot!" + entry)
+        else:
+            flash("All the form fields are required. ")
+
+        return render_template("grainnoush.html", form=form)
 
 
 if __name__ == "__main__":
