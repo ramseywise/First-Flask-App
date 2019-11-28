710e857e42a81e26f88612d838c527d734a6c496
Elizabeth Wise <elizabethwise@Elizabeth-Wise.local>
2019-11-28 11:34:16 +0100

    hehe

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello, World!"
-    
+    """Return home webpage."""
+
+    return "Hello, Grainnoush!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
