064f9e73a0aeb0f0c974ad76871f065051bf1744
Elizabeth Wise <elizabethwise@Elizabeth-Wise.local>
2019-12-01 11:18:56 +0100

    meow

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import sys
 import random
 import requests
-from unidecode import unidecode
 from io import BytesIO, TextIOWrapper
 # from flask_restplus import reqparse, Api, Resource, abort
 # from flask_restful import request
@@ -26,6 +25,13 @@ def home():
     """Return home webpage."""
 
     return render_template("home.html")
+
+
+@app.route("/haha")
+def haha():
+    """Return prank webpage."""
+
+    return render_template("haha.html")
 
 
 @app.route("/kitty")
@@ -76,13 +82,6 @@ def kitty():
     url = "http://www.randomkittengenerator.com/cats/rotator.php"
 
     return render_template("kitty.html", response=url, insert=insert)
-
-
-@app.route("/haha")
-def haha():
-    """Return prank webpage."""
-
-    return render_template("haha.html")
 
 
 @app.route("/badass")
@@ -168,172 +167,34 @@ def badass():
         "Yaaaaaasss queen",
     ]
     d = dict(zip(keys, values))
-    res = key, val = random.choice(list(d.items())) 
-    keyword = res[0].decode('utf-8')
+    res = key, val = random.choice(list(d.items()))
+    
 
-    old_stdout = sys.stdout
-    sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)  
+    # old_stdout = sys.stdout
+    # sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
 
-    response = google_images_download.googleimagesdownload()
+    # response = google_images_download.googleimagesdownload()
 
-    arguments = {
-        "keywords": keyword, 
-        "limit": 1, 
-        "print_urls": True,
-    }
-    paths = response.download(arguments)
-    print(paths)
+    # arguments = {
+    #     "keywords": res[0], 
+    #     "limit": 1, 
+    #     "print_urls": True,
+    # }
+    # paths = response.download(arguments)
+    # print(paths)
 
-    sys.stdout.seek(0)
-    output = sys.stdout.read()
+    # sys.stdout.seek(0)
+    # output = sys.stdout.read()
 
-    sys.stdout.close()
-    sys.stdout = old_stdout
+    # sys.stdout.close()
+    # sys.stdout = old_stdout
+    
+    # for line in output.split("\n"):
+    #     if line.startswith("Image URL:"):
+    #         image_url = line.replace("Image URL: ", "") 
 
-    for line in output.split("\n"):
-        if line.startswith("Image URL:"):
-            image_url = line.replace("Image URL: ", "")
+    return render_template("badass.html", response=results, insert=val)
 
-        return render_template("badass.html", response=image_url, insert=res[1])
-
-# image_path = '/content/gdrive/My Drive/.../'
-# image_path = '/content/downloads/'
-
-# dataset = [img_name for img_name in os.listdir(image_path + keyws + '/')]
-
-# dataset[:10] 
-
-
-
-
-@app.route("/badass2")
-def badass2():
-    """Return google search initiative webpage."""
-
-
-    keys = [
-        "Cat AND just being a dork",
-        "Cat AND territorial?",
-        "Cat AND  fearless leader",
-        "kitten AND super cute",
-        "Cat AND praise",
-        "Cat AND doctor AND sass",
-        "Cat AND Whoooop whoop",
-        "Cat AND Sneaky",
-        "Cat AND I got you boo",
-        "Cat AND Fuzzy Wuzzy",
-        "Cat AND Declean and rebase",
-        "Cat AND confused",
-        "Cat AND mischievious",
-        "Cat AND locked out",
-        "Cat AND Feel better babe!",
-        "Cat AND buckets",
-        "Cat AND thank you",
-        "YCat AND you can run, but you cant escape Macau",
-        "Cat AND Data ladies",
-        "Cat AND toilet",
-        "Cat AND DWH had a little meltdown",
-        "Cat AND Virtual hug",
-        "Cat AND Paw paw paw paw at my <3",
-        "Cat AND Fifty shades of Gondor",
-        "Cat AND My Irish Southern Belle",
-        "Cat AND Love Child",
-        "Cat AND femme fatale",
-        "Cat AND pandas",
-        "Cat AND Dungeon Demon",
-        "Cat AND FUN day",
-        "Cat AND Hey squirrel friend",
-        "Cat AND We are gonna catch us some frauds",
-        "Cat AND coding",
-        "cutest cat photo ever",
-        "Cat AND super chilled AND cute",
-        "Cat AND adorable",
-        "Cat AND  queen",
-    ]
-    values = [
-        "Just being a dork",
-        "Who are you and why are you sitting at Grainnoushes desk?",
-        "My fearless leader",
-        "Grainnnoush oush oush begoush",
-        "Well done lady!",
-        "Im a doctor bitch",
-        "Whoooop whoop",
-        "Sneaky Snakey",
-        "I got you boo",
-        "Fuzzy Wuzzy",
-        "Declean and rebase",
-        "Are we meeting now?",
-        "ourfireisbiggerthanyours",
-        "I dont have permissions",
-        "Feel better babe!",
-        "Merci buckets",
-        "Spanks",
-        "You can run, but you cant escape Macau",
-        "Data ladies, fuck yeah",
-        "Lets pp together",
-        "DWH had a little meltdown",
-        "Virtual hug",
-        "Paw paw paw paw at my <3",
-        "Fifty shades of Gondor",
-        "My Irish Southern Belle",
-        "The Macau-AML-Plumbum Love Child",
-        "You killed it, G",
-        "Panda eats, shoots and leaves",
-        "Dungeon Demon",
-        "Get stuff FUN day",
-        "Hey squirrel friend",
-        "We are gonna catch us some frauds",
-        "Bokey bokeh",
-        "OMG I love this pussy!",
-        "Prrrrrrrrrfect ",
-        "Tots adorbs",
-        "Yaaaaaasss queen",
-    ]
-    d = dict(zip(keys, values))
-    res = key, val = random.choice(list(d.items())) 
-
-    # parse command line options
-    parser = argparse.ArgumentParser()
-    parser.add_argument("keyword", help=res[0])
-    args = parser.parse_args()
-
-    # get user input and search on google
-    query = args.keyword
-    url = "https://www.google.com/search?as_st=y&tbm=isch&as_q=" + query + \
-          "&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:lt,islt:svga,itp:photo,ift:jpg"
-    source = search(url)
-
-    # Parse the page source and download pics
-    soup = BeautifulSoup(str(source), "html.parser")
-    ua = UserAgent()
-
-    # check directory and create if necessary
-    dest = "images/" + args.keyword
-    if not os.path.isdir(dest):
-        os.makedirs(dest)
-
-    # get the links
-    links = soup.find_all("a", class_="rg_l")
-    links = [link.get("href") for link in links]
-
-    # open some processes to download
-    with Pool(processes=args.worker) as pool:
-        func = partial(download_image, dest)
-        pool.map(func, links)
-
-        return render_template("badass2.html", response=links, insert=res[1])
-
-# @app.route("/form")
-# def form():
-#     """Return form submission webpage."""
-
-#     return render_template("form.html", form=form)
-
-# @app.route("/form", methods=["POST"])
-# def my_form_post():
-#     text = request.form["text"]
-#     processed_text = text.upper()
-#     return processed_text
 
 if __name__ == "__main__":
     app.run(debug=True)
